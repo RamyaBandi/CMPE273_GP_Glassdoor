8b73dca5fce2e55eda92e97036df82c010872533
cvamsik <55032250+cvamsik@users.noreply.github.com>
2020-11-15 01:46:34 -0800

    companyOne branch created

--- Backend/index.js
+++ Backend/index.js
@@ -13,7 +13,7 @@ const path = require('path');
 let mongo = require('./config/mongoConnection')
 
 
-
+//Company One branch created
 var passport = require('passport');
 var jwt = require('jwt-simple');
 app.use(passport.initialize());
