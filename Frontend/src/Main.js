272a4383db6c8388826cc4458ec7265a0148022d
Nivali Reddy <nivalireddy.sripati@sjsu.edu>
2020-11-17 13:17:19 -0800

    Updated company overview page

--- Frontend/src/Main.js
+++ Frontend/src/Main.js
@@ -1,13 +1,13 @@
 import React, { Component } from 'react';
 import { Route } from 'react-router-dom';
-import companyNavBar from './components/companyNavBar';
+import companyOverview from './components/companyOverview';
 
 //Create a Main Component
 class Main extends Component {
     render() {
         return (
             <div>
-                <Route path = "/" component = {companyNavBar} />
+                <Route path = "/overview" component = {companyOverview} />
             </div>
         )
     }
