eab405f509e33e3454917ed3c47364194572460b
cvamsik <55032250+cvamsik@users.noreply.github.com>
2020-11-26 05:56:56 -0800

    added job update and list

--- Frontend/src/components/employer/jobs/listJobs/ListCompanyJobs.jsx
+++ Frontend/src/components/employer/jobs/listJobs/ListCompanyJobs.jsx
@@ -1,15 +1,25 @@
 import React, { Component } from 'react';
 import Axios from 'axios'
-import { BACKEND_URL, JOB_ROUTE, GET_COMPANY_JOB } from '../../../../config/routeConstants'
+import { BACKEND_URL, JOB_ROUTE, GET_COMPANY_JOBS } from '../../../../config/routeConstants'
+import JobCard from './JobCard/JobCard';
 
 class ListCompanyJobs extends Component {
-    state = {}
+    state = {
+        jobsList: [],
+        limit: 10,
+        page: 1,
+    }
     componentDidMount = () => {
-        console.log(`${BACKEND_URL}${JOB_ROUTE}${GET_COMPANY_JOB}`)
-        Axios.get(`${BACKEND_URL}${JOB_ROUTE}${GET_COMPANY_JOB}`, {
-            companyId: localStorage.getItem('mongoId')
+        console.log(`${BACKEND_URL}${JOB_ROUTE}${GET_COMPANY_JOBS}`)
+        Axios.get(`${BACKEND_URL}${JOB_ROUTE}${GET_COMPANY_JOBS}`, {
+            params: {
+                companyId: localStorage.getItem('mongoId'),
+                limit: this.state.limit,
+                page: this.state.page
+            }
         })
             .then((res) => {
+                this.setState({ jobsList: [...res.data.jobs] })
                 console.log(res)
             })
             .catch((err) => {
@@ -17,9 +27,18 @@ class ListCompanyJobs extends Component {
             })
     }
     render() {
+        let renderVar
+        if (this.state.jobsList.length > 0) {
+            renderVar = this.state.jobsList.map((job, key) => {
+                return <JobCard props={{ job: job, props: this.props }} key={key} />
+            })
+        }
         return (
             <div>
+                <div class="card-deck">
 
+                    {renderVar}
+                </div>
             </div>
         );
     }
