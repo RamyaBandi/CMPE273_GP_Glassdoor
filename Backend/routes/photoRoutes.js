c4a9acc4d5dcc84b2b8c4c43ef7e5dbbeee58f85
Ramya Bandi <ramyareddybandi9@gmail.com>
2020-12-02 05:08:58 -0800

    Added Admin photos routes

--- Backend/routes/photoRoutes.js
+++ Backend/routes/photoRoutes.js
@@ -3,7 +3,7 @@ const photoRouter = express.Router();
 const photoServices = require("../servicesMongo/photoServices");
 //const {checkAuth}=require("../config/passport")
 //const photoKafkaServices = require("../servicesKafka/photoServices");
-const { GET_COMPANY_PHOTOS, POST_COMPANY_PHOTOS } = require('../config/routeConstants');
+const { GET_COMPANY_PHOTOS, POST_COMPANY_PHOTOS, GET_ALL_PHOTOS, PUT_PHOTO_APPROVE, PUT_PHOTO_REJECT } = require('../config/routeConstants');
 
 // console.log(process.env.KAFKA_SWITCH);
 if (process.env.KAFKA_SWITCH === 'true') {
@@ -11,6 +11,10 @@ if (process.env.KAFKA_SWITCH === 'true') {
 else {
     photoRouter.route(POST_COMPANY_PHOTOS).post(photoServices.postCompanyPhotos);
     photoRouter.route(GET_COMPANY_PHOTOS).get(photoServices.getCompanyPhotos);
+    photoRouter.route(GET_ALL_PHOTOS).get(photoServices.getAllPhotos);
+    photoRouter.route(PUT_PHOTO_APPROVE).put(photoServices.putPhotoApprove);
+    photoRouter.route(PUT_PHOTO_REJECT).put(photoServices.putPhotoReject);
+
 }
 
 module.exports = photoRouter;
