3449c4cace54927d5f76d3113e1d05af970c6c88
cvamsik <55032250+cvamsik@users.noreply.github.com>
2020-12-02 00:43:16 -0800

    company analytics

--- Frontend/src/components/employer/analytics/AnalyticsHome.jsx
+++ Frontend/src/components/employer/analytics/AnalyticsHome.jsx
@@ -1,12 +1,17 @@
 import React, { Component } from 'react';
-
+import ApplicantDemographics from './charts/ApplicantsDemographics';
+import JobCount from './charts/JobsCount';
+import './AnalyticsHome.styles.css'
 
 class AnalyticsHome extends Component {
     state = {}
     render() {
         return (
-            <div>
-                AnalyticsHome
+            <div className="analyticsHome">
+                <div>
+                    <JobCount />
+                    <ApplicantDemographics />
+                </div>
             </div>
         );
     }
