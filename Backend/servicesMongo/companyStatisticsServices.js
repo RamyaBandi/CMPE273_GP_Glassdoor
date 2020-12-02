aa19a3646a8343da7cf0a4889bbd4abcda504167
cvamsik <55032250+cvamsik@users.noreply.github.com>
2020-12-01 21:58:02 -0800

    demographics company

--- Backend/servicesMongo/companyStatisticsServices.js
+++ Backend/servicesMongo/companyStatisticsServices.js
@@ -128,19 +128,50 @@ module.exports.getApplicationDemographics = async (req, res) => {
                             res.status(RES_INTERNAL_SERVER_ERROR).end(JSON.stringify(err));
                         }
                         else {
-                            let races = {}
-                            let gender = {}
-                            let veteranStatus = {}
-                            let disability = {}
+                            let race = {
+                                'American Indian or Alaska Native': 0,
+                                'Asian': 0,
+                                'Black or African American': 0,
+                                'Native Hawaiian or Other Pacific Islander': 0,
+                                'White': 0,
+                                'Hispanic or Latino': 0,
+                                'Do not wish to Disclose': 0
+                            }
+                            let gender = {
+                                'Male': 0,
+                                'Female': 0,
+                                'Other': 0,
+                                'Do not wish to Disclose': 0
+                            }
+                            let veteranStatus = {
+                                'Not a Veteran': 0,
+                                'Veteran': 0,
+                                'Do not wish to Disclose': 0
+                            }
+                            let disability = {
+                                'Have a Disability': 0,
+                                'Do not have a Disability': 0,
+                                'Do not wish to Disclose': 0
+                            }
                             applications.map((app) => {
-                                console.log(app.student)
-                                let race = app.student.race
-                                // races[] = races[app.student.race] + 1
+                                // console.log(app.student[0])
+                                // console.log(app.student[0].race)
+                                race[app.student[0].race] = race[app.student[0].race] + 1
+                                gender[app.student[0].gender] = gender[app.student[0].gender] + 1
+                                veteranStatus[app.student[0].race] = veteranStatus[app.student[0].race] + 1
+                                disability[app.student[0].disability] = disability[app.student[0].disability] + 1
+
                             })
-                            console.log(JSON.stringify(races))
+                            // console.log(races)
                             console.log("Demographics Counted Successfully");
-                            console.log(applications);
-                            res.status(RES_SUCCESS).end(JSON.stringify(applications));
+                            // console.log(applications);
+                            let out = {
+                                race: race,
+                                gender: gender,
+                                veteranStatus: veteranStatus,
+                                disability: disability
+                            }
+                            res.status(RES_SUCCESS).end(JSON.stringify(out));
 
                         }
                     })
