9c71a1b19c3b4cf02655db65ee0e3ad7e047151f
Nivali Reddy <nivalireddy.sripati@sjsu.edu>
2020-12-02 23:46:50 -0800

    Photos Tab

--- Backend/servicesMongo/photoServices.js
+++ Backend/servicesMongo/photoServices.js
@@ -30,96 +30,129 @@ AWS.config.update({
     secretAccessKey: process.env.AWS_S3_SECRETACCESSKEY,
     accessKeyId: process.env.AWS_S3_ACCESSKEYID,
     region: process.env.AWS_S3_REGION
-  });
+});
 
 AWS.config.setPromisesDependency(bluebird);
-  
+
 const s3 = new AWS.S3();
 
 const uploadFile = (buffer, name, type) => {
     const params = {
-      ACL: 'public-read',
-      Body: buffer,
-      Bucket: process.env.AWS_S3_BUCKET_NAME,
-      ContentType: type.mime,
-      Key: `${name}.${type.ext}`
+        ACL: 'public-read',
+        Body: buffer,
+        Bucket: process.env.AWS_S3_BUCKET_NAME,
+        ContentType: type.mime,
+        Key: `${name}.${type.ext}`
     };
     return s3.upload(params).promise();
-  };
+};
 
 module.exports.postCompanyPhotos = async (req, res) => {
-    console.log("Inside Jobs POST service");
+    console.log("Inside Photos POST service");
     const id = req.query.id;
     //console.log(req.query)
     const form = new multiparty.Form();
-    let inData = req.body;
-    let photos = Photos({
-        studentId: '5fb48df63d242fa0842343f3',
-        companyId: id,
-        photoURL: 'xyz',
-        approvalStatus: 'true',
-        uploadDate: Date.now(),
-        //fileName: inData.fileName
-    })
-    photos.save((err, result) => {
-        if (err) {
-            console.log("Error posting photos")
-            console.log(err);
-            //res.setHeader(CONTENT_TYPE, APP_JSON);
-            res.status(RES_INTERNAL_SERVER_ERROR).end(JSON.stringify(err));
-        }
-        else {
-            Company.findOneAndUpdate({ _id: inData.companyId }, { $push: { 'photos': result._id } }, (error, results) => {
-                if (error) {
-                    console.log("Error Updating Company with photos id")
-                    console.log(error);
-                    //res.setHeader(CONTENT_TYPE, APP_JSON);
-                    res.status(RES_INTERNAL_SERVER_ERROR).end(JSON.stringify(error));
-                }
-                else {
-                    // console.log(JSON.stringify(result));
-                    //res.setHeader(CONTENT_TYPE, APP_JSON);
-                    console.log("Photos created successfully");
-                    console.log(result);
-                    res.status(RES_SUCCESS).send(result);
-                }
-            })
-        }
-    })
+    //find count
+
+    const count = await Photos.countDocuments({ companyId: id });
+    console.log(count);
     form.parse(req, async (error, fields, files) => {
-      if (error) throw new Error(error);
-      try {
-        //console.log(files);
-        //return res.status(200).send("done");
-        for (var key in files) {
-            const {path, fieldName} = files[key][0];
-            const buffer = fs.readFileSync(path);
-            const type = await fileType.fromBuffer(buffer)
-            //const timestamp = Date.now().toString();
-            const fileName = `companyPhotos/${id}/${fieldName}`;
-            //console.log(fileName)
-            const data = await uploadFile(buffer, fileName, type);
-            console.log(data);
-          }
-        return res.status(200).send("done");
-      } catch (err) {
-          console.log(err)
-          return res.status(400).send(err);
-      }
+        if (error) throw new Error(error);
+        try {
+            //console.log(files);
+            //return res.status(200).send("done");
+            for (var key in files) {
+                const { path, fieldName } = files[key][0];
+                const buffer = fs.readFileSync(path);
+                const type = await fileType.fromBuffer(buffer)
+                //const timestamp = Date.now().toString();
+                const fileName = `companyPhotos/${id}/${(parseInt(fieldName)+count).toString()}`;
+                console.log(fileName)
+                const data = await uploadFile(buffer, fileName, type);
+                //insert into db
+                let inData = req.body;
+                let photos = Photos({
+                    studentId: '5fb48df63d242fa0842343f3',
+                    companyId: id,
+                    photoURL:data.Location ,
+                    approvalStatus: 'Under Review',
+                    uploadDate: Date.now(),
+                    //fileName: inData.fileName
+                })
+
+                await photos.save((err, result) => {
+                    if (err) {
+                        console.log("Error posting photos")
+                        console.log(err);
+                        //res.setHeader(CONTENT_TYPE, APP_JSON);
+                        res.status(RES_INTERNAL_SERVER_ERROR).end(JSON.stringify(err));
+                    }
+                    else {
+                        Company.findOneAndUpdate({ _id: inData.companyId }, { $push: { 'photos': result._id } }, (error, results) => {
+                            if (error) {
+                                console.log("Error Updating Company with photos id")
+                                console.log(error);
+                                //res.setHeader(CONTENT_TYPE, APP_JSON);
+                                res.status(RES_INTERNAL_SERVER_ERROR).end(JSON.stringify(error));
+                            }
+                            else {
+                                // console.log(JSON.stringify(result));
+                                //res.setHeader(CONTENT_TYPE, APP_JSON);
+                                console.log("Photos created successfully");
+                                console.log(result);
+                                //res.status(RES_SUCCESS).send(result);
+                            }
+                        })
+                    }
+                })
+            }
+
+        //    const temp = await Photos.find({companyId: id}).skip(0).limit(12).exec();
+            return res.send("upload done").status(200);
+
+        } catch (err) {
+            console.log(err)
+            return res.status(400).send(err);
+        }
     });
 }
 
 module.exports.getCompanyPhotos = async (req, res) => {
-    console.log("Inside Company Photos GET service");
-    console.log(req.query);
+    
+    const studentId=req.query.studentId;
+    console.log(studentId)
     let data = req.query;
     try {
         data.page = 1;
-        data.limit = 10;
+        data.limit = 20;
         const photos = await Photos.find({ companyId: data.companyId }).limit(data.limit * 1).skip((data.page - 1) * data.limit).exec();
         const count = await Photos.countDocuments({ companyId: data.companyId });
+        let formattedPhotos=[]
+        for(let i of photos){
+            if(i.approvalStatus==="Rejected"){
+                continue;
+            }
+            else if (i.approvalStatus==="Under Review"){
+                
+                if (i.studentId.toString()===studentId){
+                    formattedPhotos.push({
+                        id:formattedPhotos.length,
+                        image:i.photoURL,
+                        name:i._id,
+                        approvalStatus:i.approvalStatus
+                    })
+                }
+            }else{
+                formattedPhotos.push({
+                    id:formattedPhotos.length,
+                    image:i.photoURL,
+                    name:i._id,
+                    approvalStatus:i.approvalStatus
+                })
+            }
+        }
         const result = ({
-            photos,
+            formattedPhotos,
             totalPages: Math.ceil(count / data.limit),
             currentPage: data.page
         });
