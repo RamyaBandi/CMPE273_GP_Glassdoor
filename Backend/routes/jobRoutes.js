ef6da55eb98fe5ed75155cff9a36098fb1312ff5
vihitakesiraju <kesirajuvihita@gmail.com>
2020-12-03 01:34:56 -0800

    changes

--- Backend/routes/jobRoutes.js
+++ Backend/routes/jobRoutes.js
@@ -17,7 +17,7 @@ if (process.env.KAFKA_SWITCH === 'true') {
     jobRouter.route(PUT_COMPANY_JOB).put(jobKafkaServices.updateCompanyJob);
     jobRouter.route(GET_COMPANY_JOBS).get(jobKafkaServices.getCompanyJobs);
     jobRouter.route(GET_ALL_JOBS).get(jobKafkaServices.getAllJobs);
-    jobRouter.route(GET_COMPANY_JOBS_BY_JOBTITLE_OR_CITY).get(jobKafkaServices.getCompanyJobsByJobTitleOrCity);
+    //jobRouter.route(GET_COMPANY_JOBS_BY_JOBTITLE_OR_CITY).get(jobKafkaServices.getCompanyJobsByJobTitleOrCity);
     jobRouter.route(GET_COMPANY_JOBS_BY_CITY).get(jobKafkaServices.getCompanyJobsByCity);
     jobRouter.route(GET_COMPANY_JOB_BY_JOBID).get(jobKafkaServices.getCompanyJobsByJobId);
 }
@@ -26,7 +26,7 @@ else {
     jobRouter.route(PUT_COMPANY_JOB).put(jobServices.updateCompanyJob);
     jobRouter.route(GET_COMPANY_JOBS).get(jobServices.getCompanyJobs);
     jobRouter.route(GET_ALL_JOBS).get(jobServices.getAllJobs);
-    jobRouter.route(GET_COMPANY_JOBS_BY_JOBTITLE_OR_CITY).get(jobServices.getCompanyJobsByJobTitleOrCity);
+    //jobRouter.route(GET_COMPANY_JOBS_BY_JOBTITLE_OR_CITY).get(jobServices.getCompanyJobsByJobTitleOrCity);
     jobRouter.route(GET_COMPANY_JOBS_BY_CITY).get(jobServices.getCompanyJobsByCity);
     jobRouter.route(GET_COMPANY_JOB_BY_JOBID).get(jobServices.getCompanyJobsByJobId);
 }
