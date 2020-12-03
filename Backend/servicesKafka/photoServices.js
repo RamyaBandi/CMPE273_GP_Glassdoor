4679f087e9b8bbb71cea7deec1f2b65bec484240
Nivali Reddy <nivalireddy.sripati@sjsu.edu>
2020-12-03 04:09:41 -0800

    Photos Kafka

---
+++ Backend/servicesKafka/photoServices.js
@@ -0,0 +1,44 @@
+const {
+    RES_SUCCESS,
+    RES_INTERNAL_SERVER_ERROR,
+} = require("../config/routeConstants");
+
+var kafka = require('../kafka/client');
+
+module.exports.postCompanyPhotos = async (req, res) => {
+    console.log("req.body" + JSON.stringify(req.body))
+    data = {
+        api: "POST_COMPANY_PHOTOS",
+        body: req.body
+    }
+    kafka.make_request('photos', data, function (err, results) {
+        console.log('in result');
+        console.log(results);
+        if (err) {
+            console.log("In error");
+            res.status(RES_INTERNAL_SERVER_ERROR).end(JSON.stringify(err));
+        } else {
+            console.log("In else");
+            res.status(RES_SUCCESS).send(JSON.stringify(results));
+        }
+    });
+}
+
+module.exports.getCompanyPhotos = (req, res) => {
+    console.log("req.body" + JSON.stringify(req.query))
+    data = {
+        api: "GET_COMPANY_PHOTOS",
+        body: req.query
+    }
+    kafka.make_request('photos', data, function (err, results) {
+        console.log('in result');
+        console.log(results);
+        if (err) {
+            console.log("In error");
+            res.status(RES_INTERNAL_SERVER_ERROR).end(JSON.stringify(err));
+        } else {
+            console.log("In else");
+            res.status(RES_SUCCESS).send(JSON.stringify(results));
+        }
+    });
+}
